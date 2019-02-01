35e9f4844bcc3f067bbb254412c97466359d7a9a
Caelan Murch <werer9@gmail.com>
2019-02-01 16:23:53 +0800

    Initial commit

---
+++ ESP32 Client/clientUDP.ino
@@ -0,0 +1,184 @@
+/*
+    Name: Caelan Murch
+    Project: ESP32-Client
+    Date: 19/12/2018
+
+    Project description:
+    Client that sends RSSI data from wifi card to 
+    computer based server. 
+
+    Software description: 
+    Measure RSSI of any AP that has an SSID that contains 'Measure'
+    Store the RSSI values in an array and send them to a python server
+    The server host must have an IP address of 192.168.0.118 on the Wireless network
+
+    (c) Caelan Murch
+
+*/
+
+#include <WiFi.h> // Include wifi header
+#include <WiFiUdp.h> // UDP header
+#include <cstring> // C string library
+
+// Setup wifi information
+const char *AP_SSID = "Measure7";
+const char *AP_PASS = "pass1234";
+
+const char *NET_SSID = "measure";
+const char *NET_PASS = "measure200";
+const char *NET_HOST = "192.168.0.118";
+const int NET_PORT = 8787;
+
+// Prototype functions
+int getRSSI(String SSID[], int RSSI[]);
+void sendRSSI(String SSID[], int RSSI[], int apcount);
+void connectWiFi(char *ssid, char *pass);
+void WiFiEvent(WiFiEvent_t event);
+void reconnectWifi();
+
+// buffer
+char buffer[1024];
+
+// RSSI and SSID arrays
+String SSID[100];
+int RSSI[100];
+
+// UDP client
+WiFiUDP udp;
+
+// Setup function
+void setup() 
+{
+    // Begin serial output
+    Serial.begin(115200);
+
+    // Set wifi mode
+    WiFi.mode(WIFI_AP_STA);
+
+    // Setup wifi event handler
+    WiFi.onEvent(WiFiEvent);
+
+    // Start softap
+    WiFi.softAP(AP_SSID, AP_PASS);
+    Serial.println("Started soft-ap... ");
+
+    // connect to wifi
+    connectWiFi(NET_SSID, NET_PASS);
+
+    // Init transfer buffer
+    udp.begin(WiFi.localIP(), NET_PORT);
+}
+
+// Loop function
+void loop() 
+{
+    // number of networks broadcasting
+    int count = getRSSI(SSID, RSSI);
+
+    if (count == 0) {
+        // Do nothing
+    } else {
+        // set transmit power
+        WiFi.setTxPower(WIFI_POWER_19_5dBm);
+        // begin sending UDP packet
+        udp.beginPacket(NET_HOST, NET_PORT);
+        // send message to server
+        udp.printf("TX%d Hello from Measure7\r\n", (int)WiFi.getTxPower());
+        sendRSSI(SSID, RSSI, count);
+        // end UDP packet
+        udp.endPacket();
+
+        // check size of reply 
+        int packetSize = udp.parsePacket();
+
+        if (packetSize) {
+            // Print reply to serial
+            int len = udp.read(buffer, 1024);
+            if (len > 0) {
+                buffer[len] = 0;
+            }
+            Serial.printf("Message: %s\r\n", buffer);
+        }
+    }
+}
+
+// // Get RSSI of relevant APs 
+int getRSSI(String SSID[], int RSSI[])
+{
+    // number of wifi networks
+    // TODO fix this
+    
+    // Set wifi mode
+    WiFi.mode(WIFI_AP_STA);
+    Serial.println("Scanning networks");
+    int n = WiFi.scanNetworks();
+    Serial.println("Finished scanning");
+    delay(1000);
+    int count = 0;
+    if (n <= 0 || n >= 60) {
+        Serial.println("No networks found...");
+    } else {
+        for (int i = 0; i < n; i++) {
+            // If SSID contains 'Measure', store them in arrays
+            if (strstr(WiFi.SSID(i).c_str(), "Measure") != NULL) {
+                SSID[count] = WiFi.SSID(i);
+                RSSI[count] = WiFi.RSSI(i);
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+
+// Send RSSI to server
+void sendRSSI(String SSID[], int RSSI[], int apcount)
+{
+    for (int i = 0; i < apcount; i++) {
+        udp.printf("SSID: %s RSSI: %d\r\n", SSID[i].c_str(), RSSI[i]);
+    }
+}
+
+// connect to wifi function
+void connectWiFi(const char *ssid, const char *pass) {
+    // Connect to WiFi
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    // Print network information to serial output
+    Serial.println("");
+    Serial.printf("Connected to: %s    IP Address: %s\n", 
+        WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+}
+
+// WiFi Event handler
+void WiFiEvent(WiFiEvent_t event) 
+{
+    Serial.printf("[WiFi-event] event: %d\n", event);
+
+    switch (event) {
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            WiFi.reconnect();
+            break;
+        case SYSTEM_EVENT_STA_START:
+            esp_restart();
+            break;
+        case SYSTEM_EVENT_STA_LOST_IP:
+            WiFi.reconnect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            Serial.print("Obtained IP address: ");
+            Serial.println(WiFi.localIP());
+            break;
+        case SYSTEM_EVENT_STA_STOP:
+            WiFi.reconnect();
+            break;
+        default:
+            break;
+
+    }
+}
---
+++ Server/UDPServer.py
@@ -0,0 +1,104 @@
+#!/usr/local/bin/python3
+# 
+#   Name: Caelan Murch
+#   Project: WiFiProjectServer
+#   Date: 19/12/2018
+#   
+#   Project description:
+#   Server to receive RSSI information from
+#   several MCUs, print it to the terminal real time
+#   and store the output in a text file.
+# 
+#   (c) Caelan Murch
+
+# import required libraries
+import socketserver
+import datetime
+
+
+# UDP handler from request handler subclass
+class UDPHandler(socketserver.BaseRequestHandler): 
+    
+    # process request from client
+    def handle(self):
+        # Get data from client
+        self.data = self.request[0].strip()
+        # get socket used by client
+        socket = self.request[1]
+        # print data from client in file and output
+        print("Printing from {}: ".format(self.client_address[0]))
+        print(self.data.decode('utf-8'))
+        (sender, list) = self.processData(self.data.decode('utf-8'))
+        self.logData("log", sender, list)
+        # send reply to client
+        socket.sendto("Thanks!".encode('utf-8'), self.client_address)
+
+    # bubble sort RSSI list
+    def __bubbleSort(self, array):
+        # swap array positions
+        def swap(i, j):
+            array[i], array[j] = array[j], array[i]
+        # iterate for length of array
+        count = 0
+        while count < len(array):
+            # check if every element needs to be swapped
+            for i in range(0, len(array)-1):
+                if int(array[i][0]) > int(array[i+1][0]):
+                    swap(i, i+1)
+            count = count + 1
+        return array
+
+    # process text
+    def processData(self, text):
+        # split new lines into separate arrays 
+        split = text.split('\r\n')
+        # find sender of message and tx power
+        if split[0].find('Hello from Measure') != -1:
+            sender = int(split[0][split[0].find('Hello from Measure') + 18:len(split[0])]) + \
+                100*int(split[0][split[0].find('TX')+2:split[0].find('Hello from Measure')-1])
+        # create list sorting the RSSI and SSID
+        list = []
+        for i in range(1, len(split)):
+            # if word 'Measure' found is SSID
+            if split[i].find('Measure') != -1:
+                # find integer after Measure
+                index = int(split[i][split[i].find('Measure') + 7])
+                # make it the first integer is the string follor by R
+                # and then the RSSI integer
+                list.append(str(index) + "R" + str(split[i][split[i].find('RSSI:') + 6:len(split[i])]))
+        # order list so data is in order of integer after Measure
+        list = self.__bubbleSort(list)
+        return (sender, list)
+
+    # log data
+    def logData(self, filename, sender, list):
+        # get time and date
+        now = datetime.datetime.now()
+        day = now.strftime("(%H00hrs-%d-%m-%Y)")
+        time = now.strftime("[%H:%M:%S:%f] ")
+        # create filename based on data
+        filename = filename + " - " + day + ".txt"
+        # split sender and tx power
+        tx = (sender / 100)/4.0
+        send = int(str(sender)[3:4])
+        # open log file
+        file = open(filename, "a")
+        file.write(time + "From Measure" + str(send) + "\tTX Power: {0:.1f}".format(tx) + "dBm\n")
+        # write a line with SSID and RSSI for each item in list
+        for i in range(0, len(list)):
+            file.write(time + "SSID: Measure" + list[i][0] + " RSSI: " + list[i][2:len(list[i])] + "\n")
+        # close file
+        file.close()
+
+
+# Only execute if this file is called directly
+if __name__ == "__main__":
+    # Set host as self and port as 8787
+    HOST, PORT = '', 8787
+
+    # Run server until interrupt
+    print("Starting server...")
+    with socketserver.UDPServer((HOST, PORT), UDPHandler) as server:
+        print("Server started")
+        server.serve_forever()
+        
